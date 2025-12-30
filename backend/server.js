960703158de5395edb79e6ac667e671571a1b3ec
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-12-30 23:16:31 +0530

    Refactor: Update CORS configuration to allow only production frontend origin

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,16 @@ import express from "express"
 import dotenv from "dotenv";
 import cors from "cors"
 import aiRoutes from "./routes/ai.routes.js";
-dotenv.config();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://localhost:5174",
-];
+
+dotenv.config();
 
 
 const app = express()
 
 app.use(
   cors({
-    origin: allowedOrigins, // frontend
+    origin: "https://ai-resume-builder-accio.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
@@ -27,4 +24,4 @@ app.use(express.json());
 app.use("/api/ai", aiRoutes);
 
 
-app.listen(process.env.PORT || 5000,()=>console.log(`Server Running at port : ${process.env.PORT}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Server Running at port : ${process.env.PORT}`))
